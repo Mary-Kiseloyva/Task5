bd128e9fddabee7604e74c26fc09ef5919f07bdd
Mary-Kiseloyva <Yu.mariyu03@yandex.ru>
2023-05-30 13:41:30 +0300

    commit2

---
+++ ClassLibrary1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace task5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+            vehicles.Add(new Bus());
+            vehicles.Add(new Taxi());
+            vehicles.Add(new Tram());
+            foreach (Vehicle vehicle in vehicles)
+            {
+                vehicle.ReleasePassengers(5);
+                vehicle.LetPassengersIn(5);
+            }
+            {
+                
+            }
+        }
+    }
+}
